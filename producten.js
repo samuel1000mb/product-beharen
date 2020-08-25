8e5b937b664786ec02bd133fdeac3dacaea8d582
samuel1000mb <66413450+samuel1000mb@users.noreply.github.com>
2020-08-25 09:48:47 +0200

    files

---
+++ producten.js
@@ -0,0 +1,51 @@
+var products = [
+    {
+        id: 1,
+        name: "The bastard small",
+        category: 219,
+        omshrijving: 0,
+        image: "img_1.jpg"
+    },
+    {
+        id: 1,
+        name: "The bastard small",
+        category: 219,
+        omshrijving: 0,
+        image: "img_1.jpg"
+    },
+    {
+        id: 1,
+        name: "The bastard small",
+        category: 219,
+        omshrijving: 0,
+        image: "img_1.jpg"
+    },
+    {
+        id: 1,
+        name: "The bastard small",
+        category: 219,
+        omshrijving: 0,
+        image: "img_1.jpg"
+    },
+    {
+        id: 1,
+        name: "The bastard small",
+        category: 219,
+        omshrijving: 0,
+        image: "img_1.jpg"
+    },
+    {
+        id: 1,
+        name: "The bastard small",
+        category: 219,
+        omshrijving: 0,
+        image: "img_1.jpg"
+    }
+]
+function toontable()
+function bewaren()
+
+function verwijderen(btn) {
+    var row = btn.parentNode.parentNode;
+    row.parentNode.removeChild(row);
+  }
