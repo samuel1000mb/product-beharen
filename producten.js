99c99a750bf3ba44faab3642963d46992ec1582d
samuel1000mb <66413450+samuel1000mb@users.noreply.github.com>
2020-08-26 16:06:21 +0200

    changed js

--- producten.js
+++ producten.js
@@ -7,45 +7,68 @@ var products = [
         image: "img_1.jpg"
     },
     {
-        id: 1,
+        id: 2,
         name: "The bastard small",
         category: 219,
         omshrijving: 0,
         image: "img_1.jpg"
     },
     {
-        id: 1,
+        id: 3,
         name: "The bastard small",
         category: 219,
         omshrijving: 0,
         image: "img_1.jpg"
     },
     {
-        id: 1,
+        id: 4,
         name: "The bastard small",
         category: 219,
         omshrijving: 0,
         image: "img_1.jpg"
     },
     {
-        id: 1,
+        id: 5,
         name: "The bastard small",
         category: 219,
         omshrijving: 0,
         image: "img_1.jpg"
     },
     {
-        id: 1,
+        id: 6,
         name: "The bastard small",
         category: 219,
         omshrijving: 0,
         image: "img_1.jpg"
     }
 ]
-function toontable()
-function bewaren()
+
+
+  
+
+  
+function myCreateFunction() {
+  var table = document.getElementById("myTable");
+  var row = table.insertRow(0);
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+  var cell1 = row.insertCell(2);
+  var cell2 = row.insertCell(3);
+  var cell1 = row.insertCell(4);
+  var cell2 = row.insertCell(5);
+  cell1.innerHTML = "";
+  cell2.innerHTML = "";
+  cell3.innerHTML = "";
+  cell4.innerHTML = "";
+  cell5.innerHTML = "";
+  cell6.innerHTML = "";
+  
+}
+
+
 
 function verwijderen(btn) {
     var row = btn.parentNode.parentNode;
     row.parentNode.removeChild(row);
   }
+
