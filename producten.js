464c4f5b61e98a1774014286aefc96c19dda7510
samuel1000mb <66413450+samuel1000mb@users.noreply.github.com>
2020-08-31 12:00:08 +0200

    bug fix

--- producten.js
+++ producten.js
@@ -57,39 +57,7 @@ function Initialize() {
 
         let row = tbody.insertRow();
 
-<<<<<<< HEAD
 
- 
-
-function myCreateFunction() {
-  var table = document.getElementById("myTable");
-  var newrow = table.insertRow(0);
-  var cell1 = newrow.insertCell(0);
-  var cell2 = newrow.insertCell(1);
-  var cell3 = newrow.insertCell(2);
-  var cell4 = newrow.insertCell(3);
-  var cell5 = newrow.insertCell(4);
-  var cell6 = newrow.insertCell(5);
-  var cell7 = newrow.insertCell(5);
-  
-  cell1.innerHTML = "";
-  cell2.innerHTML = "";
-  cell3.innerHTML = "";
-  cell4.innerHTML = "";
-  cell5.innerHTML = "";
-  cell6.innerHTML = "";
-  cell7.innerHTML = "";
-
-  name = document.getElementById("recipient-name").value,
-  category = document.getElementById("select").value,
-  omshrijving= document.getElementById("form8").value,
-  prijs=document.getElementById("prijs").value
-  id= document.getElementById("id").value,
-  image= document.getElementById("image").value,
-  buttons=document.getElementById("buttons"); 
- 
-  
-=======
         let cell = row.insertCell();
         cell.innerHTML = product.name;
 
@@ -109,21 +77,10 @@ function myCreateFunction() {
         cell.innerHTML += '<button type="button" class="btn btn-primary" onclick="verwijderen(this)">verwijderen</button>';
         cell.innerHTML += '<button type="button" class="btn btn-primary" onclick="bewaren(this)">bewaren</button>';
     }
->>>>>>> 789bf56326ed7df21be7d6752f83e0de8141663c
+
 }
 
 function verwijderen(btn) {
     var row = btn.parentNode.parentNode;
     row.parentNode.removeChild(row);
-<<<<<<< HEAD
-  }
-
-  function toonProducts()
-  var prod=""
-  var i;
-  for (i = 0; i < products.length; i++) {
-     prod += products[i];
-  } 
-=======
 }
->>>>>>> 789bf56326ed7df21be7d6752f83e0de8141663c
